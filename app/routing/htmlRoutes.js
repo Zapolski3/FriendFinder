c07ae5aaa5ef77a5455049e00c1eef0aabab3abe
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-05-06 14:56:04 -0400

    added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -29,7 +29,7 @@ app.get("/results", function(req, res) {
   
   
 
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
